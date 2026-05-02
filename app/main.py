2f36a2a9e79cad35dbbc27f3b3495d37e33b0a08
Jonas Becker <j.becker@bytelab-soltion.de>
2026-05-02 20:31:54 +0200

    add: uvicorn in requirements

---
+++ app/main.py
@@ -0,0 +1,9 @@
+j.becker@J-MacBook ~ % curl https://cicd.j-becker.org
+<html>
+<head><title>503 Service Temporarily Unavailable</title></head>
+<body>
+<center><h1>503 Service Temporarily Unavailable</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
+j.becker@J-MacBook ~ % 
