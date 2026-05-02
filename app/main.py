dd87b6e42adaf834336265f56cbf07efe3215865
Jonas Becker <j.becker@bytelab-soltion.de>
2026-05-02 20:37:06 +0200

    fix error in main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
-j.becker@J-MacBook ~ % curl https://cicd.j-becker.org
-<html>
-<head><title>503 Service Temporarily Unavailable</title></head>
-<body>
-<center><h1>503 Service Temporarily Unavailable</h1></center>
-<hr><center>nginx</center>
-</body>
-</html>
-j.becker@J-MacBook ~ % 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
